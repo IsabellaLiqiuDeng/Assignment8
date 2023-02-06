d1f009e20e69cc669b39f028a9d14254f00ec898
Liqiu_Deng <deng0069@algonquinlive.com>
2023-02-06 09:42:34 -0500

    Lab 4 Merge branch 'master' of https://github.com/IsabellaLiqiuDeng/AndroidLabs into Lab_3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,57 +1,152 @@
 package com.example.androidlabs;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    SharedPreferences sharedPreferences;
-    EditText editPersonName;
+
+    private ArrayList<TODO> elements;
+    private MyListAdapter myAdapter;
+    TODO todo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editPersonName = (EditText) findViewById(R.id.editPersonName);
-        Button button1 = (Button) findViewById(R.id.button1);
 
-        sharedPreferences = getSharedPreferences("Lab_3", Context.MODE_PRIVATE);
-        editPersonName.setText(sharedPreferences.getString("name", ""));
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = editPersonName.getText().toString();
-                Intent intern = new Intent(MainActivity.this, NameActivity.class);
-                intern.putExtra("n", name);
-                startActivityForResult(intern, 1);
-            }
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Switch swUrgernt = findViewById(R.id.switch1);
+        Button addButton = findViewById(R.id.button1);
+
+        elements = new ArrayList<>();
+
+
+        addButton.setOnClickListener(click -> {
+
+            String listItem = editText.getText().toString();
+
+            todo = new TODO();
+            todo.setTodoText(listItem);
+            todo.setUrgent(swUrgernt.isChecked());
+
+            elements.add(todo);
+            myAdapter.notifyDataSetChanged();
+
+            editText.setText("");
+            swUrgernt.setChecked(false);
+        });
+
+        ListView myList = findViewById(R.id.myList);
+        myList.setAdapter(myAdapter = new MyListAdapter());
+
+        myList.setOnItemClickListener((parent, view, pos, id) -> {
+//            elements.remove(pos);
+//            myAdapter.notifyDataSetChanged();
+        });
+
+        myList.setOnItemLongClickListener((p, b, pos, id) -> {
+
+            View newView = getLayoutInflater().inflate(R.layout.todo, null);
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+            //            tView.setText(elements.get(pos).getTodoText());
+
+            alertDialogBuilder.setTitle("Do you want to delete this?")
+                    //What is the message:
+                    .setMessage("The selected row is: " + pos +
+                            "\n " + elements.get(pos).todoText)
+
+                    //what the Yes button does:
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        elements.remove(elements.get(pos));
+                        myAdapter.notifyDataSetChanged();
+                    })
+
+                    //What the No button does:
+                    .setNegativeButton("No", (click, arg) -> {
+                    })
+                    .setView(newView)
+                    //Show the dialog
+                    .create().show();
+            return true;
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==RESULT_CANCELED)
-        {
-            finish();
-        } else if(requestCode == RESULT_FIRST_USER){
-            editPersonName.setText("");
 
+    private class MyListAdapter extends BaseAdapter {
+
+        public int getCount() {
+            return elements.size();
+        }
+
+        public TODO getItem(int position) {
+            return elements.get(position);
+        }
+
+        public long getItemId(int position) {
+            return (long) position;
+        }
+
+        public View getView(int position, View old, ViewGroup parent) {
+
+            View newView = old;
+            LayoutInflater inflater = getLayoutInflater();
+
+            //make a new row:
+            if (newView == null) {
+                newView = inflater.inflate(R.layout.todo, parent, false);
+            }
+
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText(getItem(position).todoText);
+
+            if (getItem(position).isUrgent) {
+                tView.setBackgroundColor(Color.RED);
+                tView.setTextColor(Color.WHITE);
+            } else {
+                tView.setBackgroundColor(Color.WHITE);
+                tView.setTextColor(Color.GRAY);
+            }
+
+            //return it to be put in the table
+            return newView;
         }
     }
+}
 
-    @Override
-    protected void onPause () {
-        super.onPause();
-        SharedPreferences.Editor myEdit = sharedPreferences.edit();
-        myEdit.putString("name",editPersonName.getText().toString());
-        myEdit.commit();
+class TODO {
+
+    String todoText;
+    boolean isUrgent;
+
+    public String getTodoText() {
+        return todoText;
+    }
+
+    public void setTodoText(String todoText) {
+        this.todoText = todoText;
+    }
+
+    public boolean isUrgent() {
+        return isUrgent;
+    }
+
+    public void setUrgent(boolean urgent) {
+        isUrgent = urgent;
     }
 }
